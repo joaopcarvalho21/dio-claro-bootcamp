76a433968e24bd8832153a7b6a77152bebd90889
joaopcarvalho21 <joaopcarvalho21@GMAIL.COM>
2024-11-11 22:37:23 -0300

    Initial commit

---
+++ src/test/java/me/dio/dio_claro_bootcamp/DioClaroBootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.dio_claro_bootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioClaroBootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
